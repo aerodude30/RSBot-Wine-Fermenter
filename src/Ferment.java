0ceb95559d9e550f552cc707011d44332501ae18
Christian <alaproductions.staff@gmail.com>
2016-06-27 21:57:07 -0400

    intial commit

---
+++ src/Ferment.java
@@ -0,0 +1,8 @@
+import org.powerbot.script.rt4.ClientContext;
+/**
+ * Created by christianbartram on 6/27/16.
+ */
+public class Ferment extends Task<ClientContext> {
+
+    public Ferment
+}
---
+++ src/Fermenter.java
@@ -0,0 +1,22 @@
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script;
+import org.powerbot.script.rt6.ClientContext;
+/**
+ * Created by christianbartram on 6/27/16.
+ */
+
+
+@Script.Manifest(name = "Demo Script v3.0", description = "A Demonstration of Aeroscripts Remote Bot Controller")
+public class Fermenter extends PollingScript<ClientContext> {
+
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void poll() {
+
+    }
+}
